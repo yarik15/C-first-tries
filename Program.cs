27bedf53779ff33f23259aa4b18066ef9a55e3bd
Yarik <yaroslav.gavrilov.1200@mail.ru>
2023-12-21 18:52:50 +0300

    Количество чисел в массиве со значением от 20 до 90

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please, enter your name: ");
-string name = Console.ReadLine();
-Console.Write("hello, ");
-Console.Write(name);
+Console.Clear();
+Console.WriteLine("Please, enter the length of the array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[n];
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1,101);
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= 20 && array[i] <= 90)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"The quantity is {count}");
+Console.WriteLine(string.Join(",",array));
