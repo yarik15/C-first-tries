edab3e5a6b0317fe13bb2d3a7ebaad972f503f5c
Yarik <yaroslav.gavrilov.1200@mail.ru>
2023-12-21 17:28:26 +0300

    Hello, user

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please, enter your name: ");
+string name = Console.ReadLine();
+Console.Write("hello, ");
+Console.Write(name);
