4d5a977111f25cce4da7c937c2d117577a93e30a
Yarik <yaroslav.gavrilov.1200@mail.ru>
2023-12-21 19:07:25 +0300

    Количество четных чисел в массиве из 10 различных

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+// Console.Clear();
+// Console.WriteLine("Please, enter the length of the array: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [] array = new int[n];
+// int count = 0;
+
+// for(int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(1,101);
+// for(int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] >= 20 && array[i] <= 90)
+//     {
+//         count++;
+//     }
+// }
+
+// Console.WriteLine($"The quantity is {count}");
+// Console.WriteLine(string.Join(",",array));
+//---------------------------------------------------------------------------------------------------------------
+// Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество чётных чисел в массиве.
 Console.Clear();
-Console.WriteLine("Please, enter the length of the array: ");
-int n = Convert.ToInt32(Console.ReadLine());
+int n = 10;
 int [] array = new int[n];
 int count = 0;
 
 for(int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(1,101);
+    array[i] = new Random().Next(0,11);
 for(int i = 0; i < array.Length; i++)
 {
-    if (array[i] >= 20 && array[i] <= 90)
+    if (array[i] % 2 == 0)
     {
         count++;
     }
 }
-
-Console.WriteLine($"The quantity is {count}");
+Console.WriteLine($"The quantity of the even numbers {count}");
 Console.WriteLine(string.Join(",",array));
