3ad5b470d5488e233c262f76df57d48bf4b74e84
Yarik <yaroslav.gavrilov.1200@mail.ru>
2024-01-01 18:03:44 +0300

    Является ли строка палиндромом

--- HomeWork_6.cs
+++ HomeWork_6.cs
@@ -6,13 +6,31 @@
 //     res += array[i];
 // Console.Write(res);
 //---------------------------------------------------------------------------------------
+// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+// Console.Clear();
+// Console.WriteLine("Введите строку");
+// string res = Console.ReadLine();
+// string new_res = "";
+// for(int i = 0; i < res.Length; i++)   
+//       new_res += res.ToLower()[i];
+// Console.Write(new_res);
+// --------------------------------------------------------------------------------------
+// Задайте произвольную строку. Выясните, является ли она палиндромом.
 Console.Clear();
 Console.WriteLine("Введите строку");
 string res = Console.ReadLine();
-string new_res = "";
-for(int i = 0; i < res.Length; i++)   
-      new_res += res.ToLower()[i];
-Console.Write(new_res);
+res = res.Replace(" ","" );
+res = res.ToLower();
+for (int i = res.Length - 1; i >= 0; i--)
+    if (res[i] != res - 1 )
+    {
+        Console.Write("Строка является полиндромом");
+    }
+    else
+    {
+        Console.Write("Строка не является полиндромом");
+    }
 
 
 
