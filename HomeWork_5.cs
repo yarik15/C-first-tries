3b48ee668d8ca152034f4896d99471982b96f07c
Yarik <yaroslav.gavrilov.1200@mail.ru>
2024-01-01 16:53:30 +0300

    переворот первой и последнней строк

--- HomeWork_5.cs
+++ HomeWork_5.cs
@@ -1,33 +1,60 @@
 // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
- void InputMatrix(int[,] matrix)
+//  void InputMatrix(int[,] matrix)
+//  {
+//      for(int i = 0; i < matrix.GetLength(0); i++)
+//          for(int j = 0; j < matrix.GetLength(1); j++)
+//              matrix[i, j] = new Random().Next(0, 10);
+//  }
+//  void PrintMatrix(int[,] matrix)
+//  {
+//      for(int i = 0; i < matrix.GetLength(0); i++)
+//      {
+//          for(int j = 0; j < matrix.GetLength(1); j++)
+//              Console.Write(matrix[i, j] + "\t");
+//          Console.WriteLine();
+//      }
+//  }
+// Console.Clear();
+// Console.Write("Введите размер матрицы: ");
+// int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
+// int[,] matrix = new int[size[0], size[1]];
+// InputMatrix(matrix);
+// PrintMatrix(matrix);
+//  Console.WriteLine("Введите координаты");
+//  int a = Convert.ToInt32(Console.ReadLine());
+//  int b = Convert.ToInt32(Console.ReadLine());
+//  if (a > size[0] && b > size[1])
+//  Console.WriteLine("Такого числа нет");
+//  else
+//  {
+//  object c = matrix.GetValue(a,b);
+//  Console.WriteLine(c);
+//  }
+// ------------------------------------------------------------------------------------------------
+// Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+ var array = new int[4, 4]
+ { 
+     { 86, 90, 111, 23 },
+     { 33, 12, 4, 67 },
+     { 34, 45, 1, 23 },
+     { 0, 7, 54, 2 }
+ };
+ for(int i = 0; i < array.GetLength(1); i++)
  {
-     for(int i = 0; i < matrix.GetLength(0); i++)
-         for(int j = 0; j < matrix.GetLength(1); j++)
-             matrix[i, j] = new Random().Next(0, 10);
+     var tmp = array[3, i];
+     array[3, i] = array[0, i];
+     array[0, i] = tmp;
  }
- void PrintMatrix(int[,] matrix)
+ for(int i = 0; i < array.GetLength(0); i++)
  {
-     for(int i = 0; i < matrix.GetLength(0); i++)
-     {
-         for(int j = 0; j < matrix.GetLength(1); j++)
-             Console.Write(matrix[i, j] + "\t");
+     for(int j = 0; j < array.GetLength(1); j++)
+         {
+             Console.Write(array[i,j] + " ");
+         }
          Console.WriteLine();
-     }
  }
-Console.Clear();
-Console.Write("Введите размер матрицы: ");
-int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray(); 
-int[,] matrix = new int[size[0], size[1]];
-InputMatrix(matrix);
-PrintMatrix(matrix);
- Console.WriteLine("Введите координаты");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a > size[0] && b > size[1])
- Console.WriteLine("Такого числа нет");
- else
- {
- object c = matrix.GetValue(a,b);
- Console.WriteLine(c);
- }
+ Console.ReadKey(true);
+
