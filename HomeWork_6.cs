c9f8850ad534ea468c5b77c59f9abe715834d5c0
Yarik <yaroslav.gavrilov.1200@mail.ru>
2024-01-01 17:57:02 +0300

    Из заглавных букв в строчные

--- HomeWork_6.cs
+++ HomeWork_6.cs
@@ -1,9 +1,20 @@
+// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+// Console.Clear();
+// char [] array = new char[]{'a', 'b', 'c', 'd', 'f'};
+// string res = "";
+// for(int i = 0; i < array.Length; i++)
+//     res += array[i];
+// Console.Write(res);
+//---------------------------------------------------------------------------------------
 Console.Clear();
-char [] array = new char[]{'a', 'b', 'c', 'd', 'f'};
-string res = "";
-for(int i = 0; i < array.Length; i++)
-    res += array[i];
-Console.Write(res);
+Console.WriteLine("Введите строку");
+string res = Console.ReadLine();
+string new_res = "";
+for(int i = 0; i < res.Length; i++)   
+      new_res += res.ToLower()[i];
+Console.Write(new_res);
+
+
 
 
 
